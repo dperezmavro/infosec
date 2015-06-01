a28e1a1fda0414be9fb3942ac3a77fd9424a2066
dionisio perez-mavrogenis <dio.perezmavrogenis@mwrinfosecurity.com>
2015-06-01 09:59:54 +0100

    code is a lot nicer not

--- hacky_easter_2015/27/drag.py
+++ hacky_easter_2015/27/drag.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import binascii
+import re
+import sys
 
 messages = [
 '60c46964f83879618e2878de539f6f4a6271d716',
@@ -11,27 +13,32 @@ messages = [
 '6cda6d6df87764709c6c7bd357d361556d77']
 
 ux_messages = [binascii.unhexlify(m) for m in messages]
+expr = re.compile('^[a-z ]+$')
 
 def drag(word):
-    for z in xrange(2, 3): # set the ranges here to work with a range of messages
-        for y in xrange(z+1,z+2): #set range here as well
+    for z in xrange(2, 3): # range of loop represents messages to be xored
+        for y in xrange(z+1,z+2): # this loop will take pairwise combinations
             print "Processing messages {:d} and {:d}".format(z,y)
             m1 = ux_messages[z]
             m2 = ux_messages[y]
             test = [ord(c) for c in word]
             res = xor(m1,m2)
             for i in xrange(0,len(res)-len(test)+1):
-                #this works, don't touch/read
-                print "{:3d}: {:s}".format(i,"".join([chr(test[j]^ord(res[i+j])) for j in xrange(0,len(test))]))
+                #this loop just XORs the crib with each part of the XORed ciphertexts. sorry.
+                xor_res = "".join([chr(test[j]^ord(res[i+j])) for j in xrange(0,len(test))])
+                if expr.match(xor_res):
+                    print "{:3d}: {:s}".format(i,xor_res)
 
 def xor(str1,str2):
     return "".join([chr(ord(str1[i])^ord(str2[i])) for i in xrange(0, len(str1 if len(str1) <= len(str2) else str2))])
 
-drag("i wear a black hat")
+#drag("i wear a black hat")
+drag(sys.argv[1])
 pt_m2 = "mr bunny is the spy"
 k = xor(pt_m2, ux_messages[2])
 for ctm in ux_messages:
     print xor(ctm,k)
+
 
 #abused this : http://www.wordfind.com/contains/ags/
 #m[0] 20 
