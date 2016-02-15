a8f401f045a9efbcccf2849b4cb957fa3080a6d5
Dionisio Perez Mavrogenis <dio.perezmavrogenis@mwrinfosecurity.com>
2016-02-15 08:04:51 +0000

    added new files for play crackme 02 crackme and updated gitignore

---
+++ google_play_cme/02/Cracker.java
@@ -0,0 +1,102 @@
+import java.util.ArrayList;
+
+public class Cracker{
+	
+	public ArrayList<Tuple> set01 = new ArrayList<Tuple>();
+	public ArrayList<Tuple> set23 = new ArrayList<Tuple>();
+
+
+	public static void main(String args[]){
+		Cracker a = new Cracker();	
+	}		
+
+	public Cracker(){
+		set01 = firstClause();
+		set23 = thirdClause();
+		combinedClauses(set01, set23);
+	}
+
+	public ArrayList<Tuple> firstClause(){
+		ArrayList<Tuple> set01_t = new ArrayList<Tuple>();
+
+		for (int i = 0 ; i < 256; i ++){
+			for(int j = 0 ; j < 256; j++){
+				if ((i + j)%11 + (i + j)%13== 13){
+					set01_t.add(new Tuple(i,j));
+				}
+			}
+		}
+		//System.out.pritln(set01_t.size());
+		return set01_t;
+	}
+
+	public ArrayList<Tuple> thirdClause(){
+		ArrayList<Tuple> set23_t = new ArrayList<Tuple>();
+
+		for (int i = 0 ; i < 256; i ++){
+			for(int j = 0 ; j < 256; j++){
+				if (((i+j)/17) + (i + j) % 23 == 12){
+					set23_t.add(new Tuple(i,j));
+				}
+			}
+		}
+		return set23_t;
+	}
+
+	public void combinedClauses(ArrayList<Tuple> clause1, ArrayList<Tuple> clause3){
+		for(Tuple t1 : clause1){
+			for (Tuple t2: clause3){
+				if (
+				((t1.getA() + t1.getB())/13) + (t2.getA() + t2.getB()) % 23 == 15 &&
+				((t1.getA()+ t1.getB()) /11) + (t2.getA() + t2.getB()) % 17 == 21
+				&& inAsciiRange(t1) && inAsciiRange(t2)
+				){
+					//System.out.println("Found : " + t1.toString() + ", " + t2.toString());
+					System.out.println(getCode(t1,t2)+ ": "+ realTest(getCode(t1,t2)));
+				}
+			}
+		}
+	}
+
+	public boolean inAsciiRange(Tuple a){
+		return a.getA() < 127 && a.getA() > 31 &&
+			   a.getB() < 127 && a.getB() > 31;
+	}
+
+	public String getCode(Tuple a , Tuple b){
+		return new String( new char[]{	
+				(char) a.getA(),
+				(char) a.getB(),
+				(char) b.getA(),
+				(char) b.getB()
+				})
+		;
+	}
+
+	public boolean realTest(String paramString){
+		int k = paramString.charAt(0);
+		int j = paramString.charAt(1);
+		int m = paramString.charAt(2);
+		int n = paramString.charAt(3);
+		int i = k + j;
+		k = j + k;
+		j = m + n;
+		m = n + m;
+		int i1 = k / 13;
+		int i2 = i / 11;
+		n = j / 17;
+		if (i%11 + k%13== 13){
+			if (i1 + m % 23 == 15){
+				if (n + m % 23 == 12){
+					if (i2 + j % 17 == 21){
+						//success!
+						return true;
+					}
+				}
+			
+			}
+		}
+
+		return false;
+	}
+}
---
+++ google_play_cme/02/Tuple.java
@@ -0,0 +1,22 @@
+public class Tuple{
+	private int item_1 = -1;
+	private int item_2 = -1;
+	
+	public Tuple (int a, int b){
+		item_1 = a;
+		item_2 = b;		
+	}		
+
+	public int getA(){
+		return item_1;
+	}
+
+	public int getB(){
+		return item_2;
+	}
+
+	@Override 
+	public String toString(){
+		return item_1 + ", " + item_2;
+	}	
+}
